228ed816f7de25d62aa58d13f10fb0325023f57d
snowdream <yanghui1986527@gmail.com>
2016-06-17 01:21:56 +0800

    Fix: Enter passphrase for /home/travis/.ssh/id_rsa:

--- .travis/setenv.sh
+++ .travis/setenv.sh
@@ -5,7 +5,7 @@ set -o errexit -o nounset
 echo "1"
 # Decrypt the private key
 openssl aes-256-cbc -K $encrypted_b9113630a4c4_key -iv $encrypted_b9113630a4c4_iv
-  -in .travis/id_rsa.enc -out ~/.ssh/id_rsa -d
+  -in id_rsa.enc -out ~/.ssh/id_rsa -d
   echo "2"
   
 # Set the permission of the key
@@ -35,7 +35,7 @@ EOF
 echo "5"
 
 # Copy SSH config and known_hosts
-cp .travis/ssh_config ~/.ssh/config
+cp ssh_config ~/.ssh/config
 # cp .travis/ssh_known_hosts ~/.ssh/known_hosts
 
 # Set Git config
