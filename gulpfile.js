20e238c63090f5bdd01e2baf6349dbfcbdfc63c0
snowdream <yanghui1986527@gmail.com>
2016-06-21 00:16:50 +0800

    Add: gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,6 @@ var ghPages = require('gulp-gh-pages');
  
 gulp.task('deploy', function() {
   return gulp.src('./.deploy_git/**/*')
-    .pipe(ghPages({remoteUrl:"git@github.com:snowdream/snowdream.github.io.git",message:"Create gh-pages branch via GitHub https://travis-ci.org/"}));
+    .pipe(ghPages({remoteUrl:"git@github.com:snowdream/snowdream.github.io.git",branch:"master",message:"Create gh-pages branch via GitHub https://travis-ci.org/"}));
 });
   
